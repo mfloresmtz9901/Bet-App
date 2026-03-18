78d148d92215b5d470c5263b3ad6bebdc189f893
Moises <mfloresmtz9901@mgmail.com>
2026-03-18 14:49:11 -0600

    First commit with Django-Cookiecutter + Docker

---
+++ bet_app/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from bet_app.users.models import User
+from bet_app.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory.create()
